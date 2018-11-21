cd2c02abb3a944d7c4e28c47c39ee4a4cb781258
Jeremy Laviole <poqudrof@gmail.com>
2018-11-21 17:28:06 +0100

    Rewritten in Sinatra.

--- processes/webserver/nectar.rb
+++ processes/webserver/nectar.rb
@@ -1,28 +1,51 @@
-# http_server.rb
+# coding: utf-8
+require 'sinatra'
+require 'json'
 
-require 'socket'
-
-server = TCPServer.new 8124
-
-while session = server.accept
-  request = session.gets
-  `eye start camera` if request.include? "/nectar/camera0/start"
-  `eye stop camera` if request.include? "/nectar/camera0/stop"
-  `eye restart camera` if request.include? "/nectar/camera0/restart"
-  `eye start camera_test` if request.include? "/nectar/camera0/test"
-  `eye start camera_intrinsics` if request.include? "/nectar/camera0/intrinsics"
-  cameraStatus =  `eye i camera`
-  intrinsicsStatus = `eye i camera_intrinsics`
-
-
-  session.print "HTTP/1.1 200\r\n"
-  session.print "Content-Type: text/html\r\n"
-  session.print "\r\n"
- 
-  session.print cameraStatus.to_s
-  session.print "<br/>"
-  session.print intrinsicsStatus.to_s
-  puts request
-
-  session.close
+get '/nectar/service/:name/:action' do
+  "Nectar service: #{params[:name]} do: #{params[:action]}!"
 end
+
+
+get '/test' do
+  @toto = "Hello1 "
+  erb :process, :layout => :main
+end
+
+
+get '/nectar/info/:name' do
+  return `eye i -j` if params[:name] == "all" 
+  `eye i #{params[:name]} -j`
+end
+
+
+get '/nectar/info/:name' do
+  `eye i #{params[:name]} -j`
+end
+
+get '/nectar/camera/:action' do
+
+  `eye start camera` if params[:action].eql? "start"
+  if params[:action].eql? "info"
+    info = JSON.parse `eye i -j` 
+    info.to_s
+  end
+end
+ 
+
+# `eye start camera` if request.include? "/nectar/camera0/start"
+# `eye stop camera` if request.include? "/nectar/camera0/stop"
+# `eye restart camera` if request.include? "/nectar/camera0/restart"
+# `eye start camera_test` if request.include? "/nectar/camera0/test"
+# `eye start camera_intrinsics` if request.include? "/nectar/camera0/intrinsics"
+# cameraStatus =  `eye i camera`
+# intrinsicsStatus = `eye i camera_intrinsics`
+
+
+# session.print "HTTP/1.1 200\r\n"
+# session.print "Content-Type: text/html\r\n"
+# session.print "\r\n"
+
+# session.print cameraStatus.to_s
+# session.print "<br/>"
+# session.print intrinsicsStatus.to_s
