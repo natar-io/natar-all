d4974acd51e596055435498788d9a4936ca33a7d
Jeremy Laviole <poqudrof@gmail.com>
2018-11-29 17:21:27 +0100

    button to get the image.

--- processes/webserver/public/js/p5redis.js
+++ processes/webserver/public/js/p5redis.js
@@ -1,17 +1,5 @@
 
 
-function convertDataURIToBinary(dataURI) {
-  var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
-  var base64 = dataURI.substring(base64Index);
-  var raw = window.atob(base64);
-  var rawLength = raw.length;
-  var array = new Uint8Array(new ArrayBuffer(rawLength));
-
-  // for(i = 0; i < rawLength; i++) {
-  //   array[i] = raw.charCodeAt(i);
-  // }
-  return array;
-}
 
 var sketch = function(p) {
 
