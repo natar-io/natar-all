d9d756e37772c22f96d4a5f4ebc31e5606e5da48
Nicolas PALARD <palardnicolas@gmail.com>
2019-04-03 12:24:30 +0200

    :hammer: Global naming update - Chilitags

--- chilitags.sh
+++ chilitags.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-git clone git@forge.pole-aquinetic.net:nectar-platform/chilitags-detection-server.git
-cd chilitags-detection-server
-git checkout master
-mkdir build ; cd build
-cmake ..
-make
+git clone git@forge.pole-aquinetic.net:nectar-platform/natar-tracker-chilitags.git
+cd natar-tracker-chilitags
+if [ $? -eq 0 ]; then
+	git checkout master
+	mkdir build ; cd build
+	cmake ..
+	make
+else
+	echo -e "Exiting ..."
+fi
