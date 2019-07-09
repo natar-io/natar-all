f95ace8b6af6fd971603fc13cb8a7785d1d97127
Jeremy Laviole <poqudrof@gmail.com>
2019-07-09 16:06:33 +0200

    updates for 0.1.2c

--- processes/apps/load-configuration-from-sketchbook.sh
+++ processes/apps/load-configuration-from-sketchbook.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
 
-prog=apps/configuration-loader.jar
+# cmd=java -jar
+# prog=apps/configuration-loader.jar
+# data=$SKETCHBOOK/libraries/PapARt/data/calibration
+
+cmd=natar-app
+prog=tech.lity.rea.nectar.apps.ConfigurationLoader
 data=$SKETCHBOOK/libraries/PapARt/data/calibration
+markers=$SKETCHBOOK/libraries/PapARt/data/markers
 
 #RGB Camera
-java -jar $prog -p / -f $data/calibration-AstraS-rgb.yaml -pd -o camera0:calibration
+$cmd $prog -p / -f $data/calibration-AstraS-rgb.yaml -pd -o camera0:calibration
 
 #Depth Camera
-java -jar $prog -p / -f $data/calibration-AstraS-depth.yaml -o camera0:calibration:depth -pd 
-java -jar $prog -p / -f $data/calibration-AstraS-stereo.xml -o camera0:extrinsics:depth -m 
+$cmd $prog -p / -f $data/calibration-AstraS-depth.yaml -o camera0:depth:calibration -pd 
+$cmd $prog -p / -f $data/calibration-AstraS-stereo.xml -o camera0:depth:extrinsics -m 
 
 #Projector
-java -jar $prog -p / -f $data/projector.yaml -o projector0:calibration -pd -pr
-java -jar $prog -p / -f $data/camProjExtrinsics.xml -o projector0:extrinsics -m -i
+$cmd $prog -p / -f $data/projector.yaml -o projector0:calibration -pd -pr
+$cmd $prog -p / -f $data/camProjExtrinsics.xml -o projector0:extrinsics -m -i
 
 #Table
-java -jar $prog -p / -f $data/tablePosition.xml -m -o table:extrinsics
+$cmd $prog -p / -f $data/tablePosition.xml -m -o table:extrinsics
+
+#Markerboards
+$cmd $prog -p / -f $markers/calib1.svg -mb -o calib1
+$cmd $prog -p / -f $markers/calib-aruco2-v.svg -mb -o calib2
+$cmd $prog -p / -f $markers/A4-default.svg -mb -o a4-default
