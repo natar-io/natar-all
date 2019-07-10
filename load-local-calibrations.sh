b251aba0ed3f8d49788204b79ce2dff186647c6d
Jeremy Laviole <poqudrof@gmail.com>
2019-07-10 20:16:43 +0200

    simplificactions

--- load-local-calibrations.sh
+++ load-local-calibrations.sh
@@ -11,15 +11,13 @@ prog=tech.lity.rea.nectar.apps.ConfigurationLoader
 data=$SKETCHBOOK/libraries/PapARt/data/calibration
 markers=$SKETCHBOOK/libraries/PapARt/data/markers
 
-local_name=$1
-
 # color
-$cmd $prog -p / -f ~/calibrations/calibration-AstraS-$local_name-rgb.yaml -pd -o camera0:calibration
+$cmd $prog -p / -f ~/calibrations/calibration-AstraS-rgb.yaml -pd -o camera0:calibration
 
 # depth
-$cmd $prog -p / -f ~/calibrations/calibration-AstraS-$local_name-depth.yaml -o camera0:depth:calibration -pd
-$cmd $prog -p / -f ~/calibrations/calibration-AstraS-$local_name-stereo.xml -o camera0:depth:extrinsics -m 
+$cmd $prog -p / -f ~/calibrations/calibration-AstraS-depth.yaml -o camera0:depth:calibration -pd
+$cmd $prog -p / -f ~/calibrations/calibration-AstraS-stereo.xml -o camera0:depth:extrinsics -m 
 
 #Projector
-$cmd $prog -p / -f ~/calibrations/projector-$local_name.yaml -o projector0:calibration -pd -pr
+$cmd $prog -p / -f ~/calibrations/projector.yaml -o projector0:calibration -pd -pr
 
