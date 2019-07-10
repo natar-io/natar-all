62315511ab369bad6e9ed705cad8c462645b4b5b
Jeremy Laviole <poqudrof@gmail.com>
2019-07-10 20:26:36 +0200

    added copy to papart

--- load-local-calibrations.sh
+++ load-local-calibrations.sh
@@ -11,6 +11,8 @@ prog=tech.lity.rea.nectar.apps.ConfigurationLoader
 data=$SKETCHBOOK/libraries/PapARt/data/calibration
 markers=$SKETCHBOOK/libraries/PapARt/data/markers
 
+echo "Copy the calibrations to Natar (Redis)." 
+
 # color
 $cmd $prog -p / -f ~/calibrations/calibration-AstraS-rgb.yaml -pd -o camera0:calibration
 
@@ -21,3 +23,5 @@ $cmd $prog -p / -f ~/calibrations/calibration-AstraS-stereo.xml -o camera0:depth
 #Projector
 $cmd $prog -p / -f ~/calibrations/projector.yaml -o projector0:calibration -pd -pr
 
+echo "Copy the calibrations to PapARt." 
+cp ~/calibrations/* /usr/share/processing/modes/java/libraries/PapARt/data/calibration/
